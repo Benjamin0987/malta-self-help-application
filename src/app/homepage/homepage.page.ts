990970bad7d5905fed1ec47064630431f076a1b7
Benjamin Magro <benjamin.magro.c10304@mcast.edu.mt>
2021-06-02 14:22:56 +0200

    Malta Info Guide Application Final

--- src/app/homepage/homepage.page.ts
+++ src/app/homepage/homepage.page.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { GalleryPage } from '../gallery/gallery.page';
+import { StorageService } from '../services/storage.service';
+import { JourneyItem } from '../struct/journey';
+import { JournaldiaryPage } from '../journaldiary/journaldiary.page';
 
 @Component({
   selector: 'app-homepage',
@@ -9,8 +12,12 @@ import { GalleryPage } from '../gallery/gallery.page';
 })
 export class HomepagePage implements OnInit {
 
+  journey: JourneyItem[] = [];
+  @Input('id') id: number = null;
+  
   constructor(
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private storageService: StorageService
   ) { }
 
   ngOnInit() {
@@ -22,6 +29,20 @@ export class HomepagePage implements OnInit {
         component: GalleryPage
       });
 
+      modal.present();
+    }
+
+    async openJournal(id?: number)
+    {
+      const modal = await this.modalCtrl.create({
+        component: JournaldiaryPage,
+        componentProps: { id: id }
+      });
+  
+      modal.onWillDismiss().then(async (value) => {
+        this.journey = await this.storageService.get('journey') || [];
+      });
+  
       modal.present();
     }
 
